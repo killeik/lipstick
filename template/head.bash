8516d5a0154dfee9fa6e9d0686e8006417eb2857
Aral Balkan <mail@ar.al>
2022-08-08 10:32:51 +0100

    Update comment

--- template/head.bash
+++ template/head.bash
@@ -4,11 +4,10 @@
 #
 # Lipstick on a Pig (lipstick) Installation Script.
 #
-# 1. Adds lipstick-monitor, lipstick-update, lipstick-enable, and lipstick-disable
-#    scripts to ~/.local/bin
-# 2. Adds light and dark configuration scripts to ~/.config/lipstick
+# 1. Adds lipstick scripts to ~/.local/bin
+# 2. Adds lipstick configuration to ~/.config/lipstick
 # 3. Adds systemd unit file to ~/.config/systemd/user
-# 4. Enables the systemd service and starts it.
+# 4. Enables systemd service and starts it.
 #
 # For more information, usage instructions, and app configurations,
 # please see: https://codeberg.org/small-tech/lipstick
